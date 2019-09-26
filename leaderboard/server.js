eee9656dfbb837501bcda8d0120b926ce4b621b4
project-transmit <54302180+project-transmit@users.noreply.github.com>
2019-09-26 13:48:32 +0930

    init

---
+++ leaderboard/server.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let fs = require('fs');
+let temp;
+
+temp = fs.readFileSync('data.json');
+let data = JSON.parse(temp);
+console.log(data);
+
+console.log('server is starting');
+let app = express();
+let server = app.listen(3000, function() { console.log('listening') });
+
+
+app.get('/get', function(request, response) { response.send(data.leaderboard) });
+app.get('/update/:info', updateLeaderboard);
+
+function updateLeaderboard(request, response) {
+    let info = request.params.info.split(',');
+    data.leaderboard.forEach(function(item, index, object) {    
+        if (element[0] == info[0]) {
+            object.splice(index, 1);
+        }
+    });
+    data.leaderboard.push(info);
+    let reply = 'leaderboard updated';
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function() { console.log(reply) });
+    response.send(reply);
+}
