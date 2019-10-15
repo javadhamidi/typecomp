fc15737e6e1ef3e374ef603661698a576cf9b8d8
project-transmit <keepmeinsideplz@gmail.com>
2019-10-15 14:03:46 +1030

    ranking stuff

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -9,38 +9,30 @@ function buildHtmlTable(selector) {
         response.json().then(data => {
             console.log(data);
             myList = data.leaderboard;
-            var columns = addAllColumnHeaders(myList, selector);
 
-            for (var i = 0; i < myList.length; i++) {
-                var row$ = $('<tr/>');
-                for (var colIndex = 0; colIndex < columns.length; colIndex++) {
-                    var cellValue = myList[i][columns[colIndex]];
+            sortedList = [];
+            lastCPM = 0;
+
+            myList.forEach(element => {
+                if(element[2] < lastCPM) {
+                    sortedList.push(element);
+                } else {
+                    lastCPM = element[2];
+                    sortedList.unshift(element);
+                }
+            });
+
+            let rank = 1;
+            sortedList.forEach(element => {
+                let row$ = $('<tr/>');
+                row$.append($('<td/>').html(rank++))
+                for (var colIndex = 0; colIndex < 2; colIndex++) {
+                    var cellValue = element[colIndex];
                     if (cellValue == null) cellValue = "";
                     row$.append($('<td/>').html(cellValue))
                 }
                 $(selector).append(row$);
-            }
+            });
         }) 
     })
-}
-
-// Adds a header row to the table and returns the set of columns.
-// Need to do union of keys from all records as some records may not contain
-// all records.
-function addAllColumnHeaders(myList, selector) {
-    var columnSet = [];
-    var headerTr$ = $('<tr/>');
-
-    for (var i = 0; i < myList.length; i++) {
-        var rowHash = myList[i];
-        for (var key in rowHash) {
-            if ($.inArray(key, columnSet) == -1) {
-                columnSet.push(key);
-                headerTr$.append($('<th/>').html(key));
-            }
-        }
-    }
-    $(selector).append(headerTr$);
-
-    return columnSet;
 }
